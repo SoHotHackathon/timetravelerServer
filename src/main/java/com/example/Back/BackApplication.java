72e2d84e4252a6a5e6fafa846028a23d2fae7bd2
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-05-06 00:14:58 +0900

    초기 세팅

---
+++ src/main/java/com/example/Back/BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApplication.class, args);
+	}
+
+}
