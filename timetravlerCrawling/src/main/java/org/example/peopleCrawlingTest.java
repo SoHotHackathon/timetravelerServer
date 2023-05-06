c857f9814e23d66b68071115569fda1fb2909cb9
cobinding <cntpqls0218@gmail.com>
2023-05-06 16:43:16 +0900

    people crawling png, jpg select finish, have to upload DB

--- timetravlerCrawling/src/main/java/org/example/peopleCrawlingTest.java
+++ timetravlerCrawling/src/main/java/org/example/peopleCrawlingTest.java
@@ -5,11 +5,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
 
 public class peopleCrawlingTest {
     public static void main(String[] args) {
-        String[] people = {"플라톤", "이마누엘_칸트", "프리드리히_니체", "공자", "르네_데카르트"};
+        String[] people = {"존_폰_노이만", "빌_게이츠", "세르게이_브린", "찰스_배비지"};
 
         for (String person : people) {
             String url = "https://ko.wikipedia.org/wiki/" + person;
@@ -22,6 +31,17 @@ public class peopleCrawlingTest {
                 Element infoboxElement = doc.selectFirst("table.infobox");
                 Elements rows = infoboxElement.select("tr");
 
+                // img 태그 중에서 class 속성이 "image"인 요소 선택
+                Elements infoboxElem = doc.select(".infobox");
+
+                List<String> photos = new ArrayList<>();
+
+                for (Element infobox : infoboxElem) {
+                    Elements img = infobox.select("img[src*=upload.wikimedia.org]");
+                    for (Element e : img)
+                        photos.add(e.attr("src"));
+                }
+
                 String birth = "";
                 String imageUrl = "";
 
@@ -33,23 +53,33 @@ public class peopleCrawlingTest {
                         String value = td.text();
                         if (attribute.equals("출생")) {
                             birth = value;
-                        } else if (attribute.equals("이미지")) {
-                            Element imageElement = td.selectFirst("img");
-                            if (imageElement != null) {
-                                imageUrl = imageElement.absUrl("src");
-                            }
                         }
                     }
                 }
 
-                System.out.println("이름: " + name);
+                System.out.println("이름: " +  name);
                 System.out.println("출생: " + birth);
-                System.out.println("이미지 URL: " + imageUrl);
+
+                String target = photos.get(0);
+                System.out.println(photos);
+
+                String word = target.substring(target.length() - 3);
+
+                System.out.println(word);
+
+                if (word.equals("png")) {
+                    System.out.println(photos.get(photos.size() - 1));
+                } else {
+                    System.out.println(photos.get(0));
+                }
+
                 System.out.println();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+
 }
 
