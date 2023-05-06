8cc29c455fc496d99300cb871593f15e022fa253
youngseojeon <20wjsdudtj@gmail.com>
2023-05-07 00:25:41 +0900

    Update MemberService.java

--- src/main/java/com/example/Back/service/MemberService.java
+++ src/main/java/com/example/Back/service/MemberService.java
@@ -1,19 +1,13 @@
 package com.example.Back.service;
 
 
-import com.example.Back.controller.dto.conversationReq;
 import com.example.Back.controller.dto.memberReq;
-import com.example.Back.domain.Conversation;
 import com.example.Back.domain.Member;
-import com.example.Back.domain.Person;
 import com.example.Back.repository.MemberRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
+import org.springframework.stereotype.Service;
 
 
-
+@Service
 public class MemberService {
 
     public static MemberRepository memberRepository;
