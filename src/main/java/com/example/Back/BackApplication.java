007f16e1052a001245d01184a6579499f75757f0
kdh <engus525@naver.com>
2023-05-06 23:30:33 +0900

    gpt 답변 json 변환

--- src/main/java/com/example/Back/BackApplication.java
+++ src/main/java/com/example/Back/BackApplication.java
@@ -2,6 +2,8 @@ package com.example.Back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
